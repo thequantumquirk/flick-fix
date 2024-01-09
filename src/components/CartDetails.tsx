cdc8ab4a665e361563fc488fbb79c9e396955730
fosslover <fosslover@duck.com>
2024-01-09 23:42:05 +0530

    style: handle overflow in cart

--- src/components/CartDetails.tsx
+++ src/components/CartDetails.tsx
@@ -12,32 +12,35 @@ export default function CartDetails() {
     <div>
       {movies.length !== 0 ? (
         <div className="flex flex-col gap-1">
-          {movies.map((movie, id) => (
-            <div
-              key={id}
-              className="flex items-center gap-2 bg-foreground/10 p-2 rounded-lg"
-            >
-              <Image
-                src={`${imgUrl + movie.poster}`}
-                alt="Movie Image"
-                width={70}
-                height={20}
-                className="rounded-lg"
-              />
-              <div>
-                <h1 className="font-bold">{movie.title}</h1>
-                <p>
-                  <span>Price: </span>${movie.price}
-                </p>
+          <div className="flex flex-col gap-1 max-h-96 overflow-y-scroll">
+            {movies.map((movie, id) => (
+              <div
+                key={id}
+                className="flex items-center gap-2 bg-foreground/10 p-2 rounded-lg"
+              >
+                <Image
+                  src={`${imgUrl + movie.poster}`}
+                  alt="Movie Image"
+                  width={70}
+                  height={20}
+                  className="rounded-lg"
+                />
+                <div>
+                  <h1 className="font-bold">{movie.title}</h1>
+                  <p>
+                    <span>Price: </span>${movie.price}
+                  </p>
+                </div>
+                <button onClick={() => removeMovies(movie.id)}>
+                  <XCircle />
+                </button>
               </div>
-              <button onClick={() => removeMovies(movie.id)}>
-                <XCircle />
-              </button>
-            </div>
-          ))}
+            ))}
+          </div>
           <div className="flex items-center justify-between">
             <p className="font-bold">
-              Total: <span className="font-normal">${totalValue}</span>
+              Total:{" "}
+              <span className="font-normal">${totalValue.toFixed(2)}</span>
             </p>
             <Button>Checkout</Button>
           </div>
